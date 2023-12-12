352638a8b681233dd0b3983fdeed36f01fb17f2f
Zion Griffin <defendingmymaininbox@gmail.com>
2023-12-11 22:28:50 -0500

    Got /api/notes working

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+/*
+WHEN I enter a new note title and the note’s text
+THEN a "Save Note" button and a "Clear Form" button appear in the navigation at the top of the page
+WHEN I click on the Save button
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes and the buttons in the navigation disappear
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column and a "New Note" button appears in the navigation
+WHEN I click on the "New Note" button in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column and the button disappears */
+
 const express = require('express');
 const path = require('path');
-
+const notes = require('./db/db.json');
 const app = express();
 const PORT = 3001;
 
@@ -12,7 +22,9 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
+app.get('/api/notes', (req, res) => 
+    res.status(200).json(notes)
+);
 app.listen(PORT, () => 
     console.log("Listening at port 3001")
 );
